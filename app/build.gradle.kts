2d105c4f73117ad0ec05c3ee0dae2f3edf81d3d6
ShinyDiscoBallsDev <310330679+ShinyDiscoBallsDev@users.noreply.github.com>
2026-08-06 15:45:25 -0400

    Updated version 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.shinydiscoballsdev.kifossk"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     applicationVariants.all {
